20600db46a29a62f3e8dd00bf01b83f15985e20d
Moustapha <moustaphamukwini@gmail.com>
2026-06-19 12:28:28 +0200

    handle click on the grid and draw makers

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from pygame.locals import *
-from grid import draw_grid
+from grid import draw_grid, draw_makers, makers
 
 screen = pygame.display.set_mode((300, 300))
 pygame.display.set_caption("TicTacToe")
 pygame.init()
 
-
+player = 1  # Player 1 starts
+pos = []
+clicked = False
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-            
+        elif event.type == MOUSEBUTTONDOWN and clicked == False:
+            clicked = True
+        elif event.type == MOUSEBUTTONUP and clicked == True:
+            clicked = False
+            pos = pygame.mouse.get_pos()
+            if makers[pos[1] // 100][pos[0] // 100] == 0:
+                makers[pos[1] // 100][pos[0] // 100] = player
+                player *= -1
+
     draw_grid(screen)
+    draw_makers(screen)
     pygame.display.update()
 
 pygame.quit()
