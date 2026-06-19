260611a0565533a1615e3772791f548138c0a9b7
Moustapha <moustaphamukwini@gmail.com>
2026-06-19 12:05:05 +0200

    create the grid

---
+++ grid.py
@@ -0,0 +1,9 @@
+import pygame
+
+def draw_grid(screen : pygame.Surface):
+    bg = (255, 255, 200)  # White background
+    screen.fill(bg)
+    grid = (50, 50, 50)  # Dark gray grid lines
+    for x in range(1, 3):
+        pygame.draw.line(screen, grid, (0, x * 100), (screen.get_width(), x * 100), 5)
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen.get_height()), 5)
