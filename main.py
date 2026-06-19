df9ce772b9a361c0059b59a3ca635962a85cc0ce
Moustapha <moustaphamukwini@gmail.com>
2026-06-19 12:10:27 +0200

    draw the grid to the screen

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from pygame.locals import *
+from grid import draw_grid
 
 screen = pygame.display.set_mode((300, 300))
 pygame.display.set_caption("TicTacToe")
 pygame.init()
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-
+            
+    draw_grid(screen)
     pygame.display.update()
 
 pygame.quit()
